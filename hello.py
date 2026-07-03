72fb203874fc318216433dd1ff4e33faadcb6721
priyanga-T20 <morallife89@gmail.com>
2026-07-03 11:10:37 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,22 @@
+print("hello world")
+a=20
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a/b)
+x=256
+print("square root=",256**0.5)
+base=5
+height=35
+print ("area of triangle =",0.5*5*35)
+kilometer=1
+miles=0.63
+print("distance in miles =",miles)
+celcius=10
+print("celcius to fahenheit =",(celcius*1.8)+32)
+temp=a
+a=b
+b=temp
+print("third variable=",a,b)
