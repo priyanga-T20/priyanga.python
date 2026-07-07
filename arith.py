562d52f0e2cd76baefb8a59365e86ea29e4c9104
priyanga-T20 <morallife89@gmail.com>
2026-07-07 11:14:35 +0530

    Add files via upload

---
+++ arith.py
@@ -0,0 +1,44 @@
+a=11
+b=2
+print(a+b)
+print (20 +b)
+print (a-b)
+print(a%b)
+print(a*b)
+print(a//b)
+x=10
+x=x+2
+x+=10
+x=20
+x*=2
+x/=1
+m=10
+n=20
+print(m==n)
+print (m>n)
+print(m<=n)
+print(m>=n)
+print(a>=10 and b<=2)
+print(a>=10 or b<=2)
+print(not a>=10)
+a=[1,2,3,4,5,8]
+print(6 in a)
+print(8 in a)
+print(2 not in  a)
+m=2
+n=2
+print(m is n)
+z=10
+print (x is z)
+print(x  is not  z)
+print (5&3)
+print (5^3)
+print (5/3)
+print (5<<2)
+print (5>>2)
+n=10
+print (~n)
+n=-10
+print (~n)
+
+
