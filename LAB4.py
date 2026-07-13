3a9e7f42f9dee348f74000017ab0bf6d5ad904f1
priyanga-T20 <morallife89@gmail.com>
2026-07-13 11:18:57 +0530

    Add files via upload

--- LAB4.py
+++ LAB4.py
@@ -1,6 +1,86 @@
 #12
 a=int(input("enter  computer marks of the student "))
-if a<=50:
-print("PASS")
+if a>=50:
+ print("PASS")
 else :
-print("FAILs")
+ print("FAIL")
+ #13
+num=int (input("enter the number:"))
+if num %10==0:
+    print("it is divisible by 10")
+else:
+    print("it is  not divisible by 10")
+ #14
+ n=int (input("enter the number:"))
+x=n%10
+y=num//10
+if x>y
+    print("it is BIGGER",x)
+else:
+    print("it is  BIGGER",y)
+  #15
+exam=int (input("enter the number:"))
+if exam==1:
+    print("it is exam is easy")
+else:
+    print("it is  exam is difficult")
+#16
+    choice=int (input("enter the number:"))
+if choice==1:
+    print("you can go out and play")
+else:
+    print("you cannot go and play")
+#17
+l=int (input("enter the  length:"))
+b=int (input("enter the breadth"))
+if l==b :
+    print("it is square")
+else:
+    print("it is  rectangle")
+    
+#18
+u=int(input("enter the ASCII value of uppercase "))
+if u>=65 and u<=90:
+ print("it is UPPERCASE")
+else :
+ print("NOT UPPERCASE")
+ #19
+l=int(input("enter the ASCII value of  lowercase "))
+if l>=97 and l<=122:
+ print("it is LOWERCASE")
+else :
+ print("NOT LOWERCASE")
+#20
+n=int(input("enter the ASCII value of numeric character "))
+if n>=48 and n<=57:
+ print("it is NUMERIC CHARACTER")
+else :
+ print("NOT  NUMERIC CHARACTER")
+#21
+m=int (input("enter the number:"))
+if m%5==0 and m%3==0:
+    print("it is divisible by 5 and 3")
+else:
+    print("it is not divisible by 5 and 3")
+#23
+num=int (input("enter the three digit number:"))
+mod=num//10
+if mod<1 and num%10==0:
+    print("it is three digit number and also multiple of 10")
+else:
+    print("it is not three digit number and also  not multiple of 10 ")
+#24
+x=int(input("enter a num:"))
+y=int(input("enter a num:"))
+if x%2==0 and y%2==0:
+    print (" the product :",x,y)
+else:
+    print(" the sum:",x,y)
+#25
+num=int(input("enter a num:"))
+dig=num%10
+div=num%7
+if dig==7 or div==0:
+    print("it is buzz number")
+else:
+    print("it is not buzz number")
